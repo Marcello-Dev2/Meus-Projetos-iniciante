1ede6ca558e8d9d559b14707131a053864469d2f
MarcelloDev <marcelodevgithub@gmail.com>
2025-07-31 09:21:37 -0300

    ajustate de css

--- javaScript/script.js
+++
@@ -1,8 +0,0 @@
-
-const txt = document.querySelector('input');
-const contador = document.getElementById('num');
-
-txt.addEventListener("keyup", function() {
-    const quantidade = txt.value.length;
-    contador.textContent = "Você Digitou: " + quantidade + "caracteres";
-});
