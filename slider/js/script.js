15b22417c439d949cd9e309485889507edd54255
MarcelloDev <marcelodevgithub@gmail.com>
2025-09-18 12:03:05 -0300

    1

--- slider/js/script.js
+++ slider/js/script.js
@@ -11,16 +11,24 @@ btnBack.onclick = () => moveItensOnClick("back")
 
 function moveItensOnClick(type) {
     let listItens = document.querySelectorAll(".list .list-item")
-    let thumbitens = document.querySelectorAll(".thumb .thumb-item")
+    let thumbItens = document.querySelectorAll(".thumb .thumb-item")
 
     console.log(listItens)
-    console.log(thumbitens)
+    console.log(thumbItens)
 
     if (type === "next") {
         list.appendChild(listItens[0])
-        thumb.appendChild()
+        thumb.appendChild(thumbItens[0])
+        container.classList.add("next")
     }else{
-        list.prepend(listItens[3])
+        list.prepend(listItens[listItens.length -1 ])
+        thumb.prepend(thumbItens[listItens.length - 1 ])
+        container.classList.add("back")
     }
+    setTimeout(() => {
+        container.classList.remove("next")
+        container.classList.remove("back")
+
+    }, 3000);
 
 }
